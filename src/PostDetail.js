9e3ed38bf60f33080e6a50195a53fb59524881d4
G H <specht.hartweger@hotmail.de>
2017-10-12 19:56:43 +0200

    5 SPECIFICATIONS REQUIRE CHANGES IN README4

--- src/PostDetail.js
+++ src/PostDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as App from './App'
+import {Link} from 'react-router-dom';
 
 
 class PostDetail extends Component { 
@@ -19,9 +20,11 @@ class PostDetail extends Component {
                 return(
                 <div key={Date.now()+4}>
                   <div key={Date.now()+ind+5}><h3>Post:</h3></div>
+                  <div className="absolute"><Link to='/' >back</Link></div>
                   <div className="post-title" key={Date.now()+ind}>{'Title: '+this.props.pro.post[e].title}</div>
                   <div className="post-author" key={Date.now()+ind+1}>{'Author: '+this.props.pro.post[e].author}</div>
                   <div className="post-time" key={Date.now()+ind+2}>{'TimeStamp: '+App.getFormattedDate(this.props.pro.post[e].timestamp)}</div>
+                  <div className="post-time" key={Date.now()+ind+6}>{'VoteScore: '+this.props.pro.post[e].voteScore}</div>  
                   <div className="post-body" key={Date.now()+ind+3}>{'Body: '+this.props.pro.post[e].body}</div>  
                  </div>
                 )
@@ -36,8 +39,8 @@ class PostDetail extends Component {
                   if(e===this.props.pro.location.pathname.substr(this.props.pro.location.pathname.indexOf("/",2)+1)){
                     z.push(
                       <div key={Date.now()+u}>
-                      <h3>Comments:</h3>
-                      <div className="post-title" key={Date.now()+u+1}>{"Body: "+this.props.pro.comments[e][u].body+" Author: "+this.props.pro.comments[e][u].author+" TimeStamp: "+App.getFormattedDate(this.props.pro.comments[e][u].timestamp)}</div>
+                      <h3>Comment {u+1}:</h3>
+                      <div className="post-title" key={Date.now()+u+1}>{"Body: "+this.props.pro.comments[e][u].body+" Author: "+this.props.pro.comments[e][u].author+" TimeStamp: "+App.getFormattedDate(this.props.pro.comments[e][u].timestamp)+" VoteScore: "+this.props.pro.comments[e][u].voteScore}</div>
                       </div>)
 
                   }
